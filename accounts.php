1bb19e76070aa38e15449846ee0779f7f1c265aa
Ankit P Shah <aps86@afsconnect2.njit.edu>
2017-11-19 17:53:38 -0500

    added files

--- accounts.php
+++ accounts.php
@@ -1,4 +1,5 @@
 <?php
+//include 'collection.php';
 class accounts extends collection {
   protected static $modelName = 'account';
 }
---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+class Manage{
+  public static function autoload($class){
+    include $class.'.php';
+  }
+}
+
+spl_autoload_register(array('Manage','autoload'));
+
+
+ini_set('display_errors', 'On');
+error_reporting(E_ALL);
+
+//require 'accounts.php';
+$obj=new main();
+
+class main{
+
+  public function __construct(){
+     echo 'Existing Accounts Records.<br>';
+     accounts::findAll();
+     echo '<br>';
+	}
+}
+?>
