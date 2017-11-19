e0276ec32a40caaf95d4f4bc4394fa49a6b70d98
Ankit P Shah <aps86@afsconnect2.njit.edu>
2017-11-19 18:13:23 -0500

    added files

--- index.php
+++ index.php
@@ -21,6 +21,42 @@ class main{
      echo 'Existing Accounts Records.<br>';
      accounts::findAll();
      echo '<br>';
+     echo 'Creating new id 26 in accounts table.<br>';
+     $record = new account();
+     $record->id='';
+     $record->email='jbl@gmail.com';
+     $record->fname='jbl';
+     $record->lname='qpz';
+     $record->phone='975-535-5019';
+     $record->birthday='2001-07-06';
+     $record->gender='male';
+     $record->password='125445';
+     //$record->save();
+     echo 'After adding record.<br>';
+     accounts::findAll();
+     echo '<br>';
+  
+      echo 'Searching new created id 26 in accounts table.<br>';
+     $id=26;
+     accounts::findOne($id);
+     
+     echo '<br>'
+     echo 'updating details of id=26.<br>';
+     $record = new account();
+     $record->id=26;
+     $record->email='skl@gmail.com';
+     $record->fname='qeq';
+     $record->lname='kkl';
+     $record->phone='923-345-1111';
+     $record->birthday='1999-08-03';
+     $record->gender='male';
+     $record->password='12367';
+     $record->save();
+     echo 'After update.<br>';
+     accounts::findOne($id);
+     echo '<br>';
+  //print_r($record1);
+    
 	}
 }
 ?>
